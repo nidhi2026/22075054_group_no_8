d96f2d25310c14c5f9b767eb77f4dc9f0751fb68
knighthinata <nidhi.student.cse22@itbhu.ac.in>
2023-11-24 16:06:08 +0530

    update requirements.txt

--- realm/settings.py
+++ realm/settings.py
@@ -82,7 +82,7 @@ DATABASES = {
         'ENGINE': 'django.db.backends.mysql',
         'NAME': 'kiwi',
         'USER': 'root',
-        'PASSWORD': your_password,
+        'PASSWORD': "madras@1234",
         'HOST': 'localhost',  
         'PORT': '3306',
     }
